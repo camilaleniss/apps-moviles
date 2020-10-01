f58e762bb53b2c5bf02518477669c6c5f2f8af79
camilaleniss <mlmariacami2@gmail.com>
2020-09-30 21:24:23 -0500

    Added permission to open camera and camera functionality to take a photo and show it

--- PermisosDemo/app/src/main/java/com/example/permisosdemo/MainActivity.java
+++ PermisosDemo/app/src/main/java/com/example/permisosdemo/MainActivity.java
@@ -1,24 +1,45 @@
 package com.example.permisosdemo;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final int PERMISSIONS_CALLBACK = 11;
+    public static final int CAMERA_CALLBACK = 12;
 
     private Button btnOpenCamera;
     private Button btnOpenGallery;
     private Button btnDownloadImg;
+
     private EditText txtImgURL;
+
+    private ImageView imgMain;
+
+    private File file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +50,17 @@ public class MainActivity extends AppCompatActivity {
         btnOpenGallery = findViewById(R.id.btnOpenGallery);
         btnDownloadImg = findViewById(R.id.btnDownloadImg);
         txtImgURL = findViewById(R.id.txtImageURL);
+        imgMain = findViewById(R.id.imgMain);
 
         ActivityCompat.requestPermissions(this, new String[]{
                 Manifest.permission.CAMERA,
                 Manifest.permission.READ_EXTERNAL_STORAGE,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
         }, PERMISSIONS_CALLBACK);
+
+        btnOpenCamera.setOnClickListener(this);
+        btnOpenGallery.setOnClickListener(this);
+        btnDownloadImg.setOnClickListener(this);
     }
 
     @Override
@@ -56,6 +82,44 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "Not allowed, PANIC", Toast.LENGTH_LONG).show();
             }
 
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnOpenCamera:
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                file = new File(getExternalFilesDir(null)+"/photo.png");
+                Log.e(">>>>>",""+file);
+
+                Uri uri = FileProvider.getUriForFile(this, getPackageName(), file);
+
+                startActivityForResult(intent, CAMERA_CALLBACK);
+
+                break;
+            case R.id.btnOpenGallery:
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==CAMERA_CALLBACK && resultCode== RESULT_OK){
+            Bitmap image = BitmapFactory.decodeFile(file.getPath());
+            Bitmap thumbnail = Bitmap.createScaledBitmap(
+                    image, image.getWidth()/4, image.getHeight()/4, true
+            );
+
+            Matrix matrix = new Matrix();
+            matrix.postRotate(90);
+            Bitmap rotatedBitMap = Bitmap.createBitmap(thumbnail, 0, 0,
+                    thumbnail.getWidth(), thumbnail.getHeight(), matrix, true);
+
+            imgMain.setImageBitmap(rotatedBitMap);
         }
     }
 }
